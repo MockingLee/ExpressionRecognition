4f800838d6869be1372427fb6ee96ff82b95e80b
MockingLee <38075220+MockingLee@users.noreply.github.com>
2019-04-20 18:21:20 +0800

    web service finished

--- RealPicTest.py
+++ RealPicTest.py
@@ -1,22 +1,27 @@
 import cv2
 import keras
 import numpy as np
-pic = cv2.imread("./40CC25C74C506A4522CDB4610794A6BC.jpg")
 
-pic = cv2.resize(pic, (48,48))
 
-gray = np.array(cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY))
+def getPrediction(model , path):
+    print(path)
+    pic = cv2.imread(path)
+    pic = cv2.resize(pic, (48, 48))
+    gray = np.array(cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY))
+    X = gray
+    X = X.reshape(1, 48, 48, 1)
+    print(X.shape)
+    score = model.predict(X)
+    li = ["Angry", "Disgust", "Fear", "Happy", "Sad", "Surprise", "Neutral"]
+    return li[np.argmax(score)]
 
-model = keras.models.load_model(filepath="./Model.299-0.6065.hdf5")
-
-X = gray
-
-X = X.reshape(1,48 , 48 ,1)
-print(X.shape)
-
-score = model.predict(X)
-print(np.argmax(score))
 
 """
 0=Angry, 1=Disgust, 2=Fear, 3=Happy, 4=Sad, 5=Surprise, 6=Neutral
 """
+
+# if __name__ == '__main__':
+#     model = keras.models.load_model(
+#         filepath="model_file/Model.299-0.6065.hdf5")
+#
+#     print(getPrediction(model ,r"C:\Users\18140\Documents\GitHub\ExpressionRecognition\WebService\fileDir\1555753532.5165482_angry.jpg"))
---
+++ WebService/run.py
@@ -0,0 +1,58 @@
+import os
+import cv2
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import time
+import keras
+import numpy as np
+import tensorflow as tf
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'fileDir'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+global graph
+graph = tf.get_default_graph()
+model = keras.models.load_model(filepath="../model_file/Model.299-0.6065.hdf5")
+
+@app.route('/index')
+def hello_world():
+    return render_template("index.html")
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+@app.route('/fileUpload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        #print(request.files)
+        file = request.files['uploaded_image']
+        print(type(file))
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            new_filename = str(time.time()) + "_" + filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], new_filename))
+            #res = getPrediction(model , os.path.join(r"C:\Users\18140\Documents\GitHub\ExpressionRecognition\WebService\fileDir" , new_filename))
+            pic = cv2.imread(UPLOAD_FOLDER + "/" + new_filename)
+            pic = cv2.resize(pic, (48, 48))
+            gray = np.array(cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY))
+            X = gray
+            X = X.reshape(1, 48, 48, 1)
+            # print(X.shape)
+            # print(model)
+            with graph.as_default():
+                score = model.predict(X)
+            li = ["Angry", "Disgust", "Fear", "Happy", "Sad", "Surprise", "Neutral"]
+            return li[np.argmax(score)]
+    return "Wrong Request"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
+
+
+
